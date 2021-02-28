3d2f445e64fdf99e8d667205310b89d393c0d786
Alexander Horn <alexomanie@gmail.com>
2021-02-28 17:34:28 +0100

    encrypt local storage

--- voting-client/src/store.js
+++ voting-client/src/store.js
@@ -1,6 +1,23 @@
 import create from "zustand"
 import produce from "immer"
 import { persist } from "zustand/middleware"
+import SecureLS from 'secure-ls'
+
+class mySecureLs {
+  constructor() {
+    this.secureLs = new SecureLS()
+    this.getItem = this.getItem.bind(this)
+    this.setItem = this.setItem.bind(this)
+  }
+  getItem(key) {
+    return this.secureLs.get(key);
+  }
+  setItem(key, data) {
+    return this.secureLs.set(key, data);
+  }
+}
+
+const secLs = new mySecureLs()
 
 export const useStore = create((set, get) => ({
   questions: [],
@@ -30,5 +47,8 @@ export const useVotedQuestionsStore = create(
   persist((set) => ({
     votedQuestionIds: [],
     set: (fn) => set(produce(fn)),
-  }))
+  }), {
+    name: 'questionStorage',
+    getStorage: () => secLs,
+  })
 )
